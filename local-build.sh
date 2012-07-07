0e3107dda4f473ef5fd131077a3979c6c52d716d
Kris De Volder <kris.de.volder@gmail.com>
2012-07-06 17:36:14 -0700

    Restructure the build to follow the example of tc-server tooling builds.

--- local-build.sh
+++ local-build.sh
@@ -3,4 +3,4 @@
 #This is the command that I run when testing the build locally
 #mvn -Dmaven.test.skip=true -Dp2.qualifier=M2 -Pe37 clean install
 #mvn -Dp2.qualifier=M2 -Pe37 clean install
-mvn -Dp2.qualifier=M2 -Pe37 clean install
+mvn -Dp2.qualifier=M3 -Pe42 clean install
