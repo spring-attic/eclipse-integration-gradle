66f8b1c1f6188dd9f26ed9dffd44cc25b2263088
Kris De Volder <kris.de.volder@gmail.com>
2012-07-09 13:21:45 -0700

    Remove obsolete 'common' subproject/folder

--- common/README.txt
+++
@@ -1,3 +0,0 @@
-All the stuff in this folder is 'defunct'.
-No longer used after build restructuring. 
-It will/should be removed shortly.
--- common/sign.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -e "$HOME/.keytool/springsource.jks" ]
-then
-	echo Signing $1
-	#jarsigner -tsa https://timestamp.geotrust.com/tsa -keystore ~/.keytool/springsource.jks -keypass $KEY_PASSWORD -storepass $STORE_PASSWORD $1 vmware
-	jarsigner -keystore ~/.keytool/springsource.jks -keypass $KEY_PASSWORD -storepass $STORE_PASSWORD $1 vmware
-fi
