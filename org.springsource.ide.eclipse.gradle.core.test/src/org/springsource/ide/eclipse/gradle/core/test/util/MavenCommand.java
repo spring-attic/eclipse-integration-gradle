9444788c8543aeebf2d2a257ab616ec7c94bef1c
Kris De Volder <kdvolder@vmware.com>
2012-08-17 12:12:09 -0700

    Another attempt to fix how we run mvn commands from the Gradle regression tests

--- org.springsource.ide.eclipse.gradle.core.test/src/org/springsource/ide/eclipse/gradle/core/test/util/MavenCommand.java
+++ org.springsource.ide.eclipse.gradle.core.test/src/org/springsource/ide/eclipse/gradle/core/test/util/MavenCommand.java
@@ -10,20 +10,44 @@
  *******************************************************************************/
 package org.springsource.ide.eclipse.gradle.core.test.util;
 
+import java.io.File;
 import java.util.Map;
 
 public class MavenCommand extends ExternalCommand {
 
+	public static File mvnHome = null;
+	static {	
+		//This code is specific to our build server but without,
+		//it seems like we cannot correctly execute mvn commands
+		//on the build server which has multiple versions of maven installed.
+		File f = new File("/opt/java/tools/maven/apache-maven-3.0.3");
+		if (f.exists()) {
+			mvnHome = f;
+		}
+	}
+
 	public MavenCommand(String... pieces) {
-		super(pieces);
+		super(customize(pieces));
+	}
+
+	private static String[] customize(String[] pieces) {
+		if (mvnHome!=null) {
+			pieces[0] = mvnHome+"/bin/mvn";
+		}
+		return pieces;
 	}
 
 	@Override
 	public void configure(ProcessBuilder processBuilder) {
 		super.configure(processBuilder);
 		Map<String, String> env = processBuilder.environment();
-		env.remove("M2_HOME"); //See http://askubuntu.com/questions/41017/maven-exits-after-trying-to-run-it
-			//Apparantly having this set causes maven commands to fail on the build servers.
+		if (mvnHome!=null) {
+			env.put("M2_HOME", mvnHome.toString());
+			env.put("MAVEN_HOME", mvnHome.toString());
+			env.put("MAVEN2_HOME", mvnHome.toString());
+		} else {
+			System.out.println("Warning: couldn't find a maven 3.0.3");
+		}
 	}
 	
 }
