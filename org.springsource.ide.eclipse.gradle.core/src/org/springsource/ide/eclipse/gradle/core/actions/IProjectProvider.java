ec10715772007f999bbbcfb6908714a495ee1de2
Martin Lippert <mlippert@gopivotal.com>
2013-09-20 14:29:20 +0200

    changing copyright statements to GoPivotal and removing duplicated spaces in comments

--- org.springsource.ide.eclipse.gradle.core/src/org/springsource/ide/eclipse/gradle/core/actions/IProjectProvider.java
+++ org.springsource.ide.eclipse.gradle.core/src/org/springsource/ide/eclipse/gradle/core/actions/IProjectProvider.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2013 VMWare, Inc.
+ * Copyright (c) 2013 GoPivotal, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * VMWare, Inc. - initial API and implementation
+ * GoPivotal, Inc. - initial API and implementation
  *******************************************************************************/
 package org.springsource.ide.eclipse.gradle.core.actions;
 
