aeb368250003d7a67cd64db0fc6cba69dfd6e5a4
Martin Lippert <mlippert@gopivotal.com>
2014-01-22 11:19:57 +0100

    updated company legal name to Pivotal Software Inc.

--- org.springsource.ide.eclipse.gradle.core.test/resources/projects/multiproject-m6/boink/Main.java
+++ org.springsource.ide.eclipse.gradle.core.test/resources/projects/multiproject-m6/boink/Main.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2012 GoPivotal, Inc.
+ * Copyright (c) 2012 Pivotal Software, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * GoPivotal, Inc. - initial API and implementation
+ * Pivotal Software, Inc. - initial API and implementation
  *******************************************************************************/
 public class Main {
 	public static void main(String[] args) {
