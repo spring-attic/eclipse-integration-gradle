fe66835182c6f6f5f2b072ca4e25c5821431b356
Kris De Volder <kdevolder@pivotal.io>
2014-11-28 13:56:52 -0800

    Improve non-exported dependencies test project

---
+++ org.springsource.ide.eclipse.gradle.core.test/resources/projects/non-exported-deps/lib/src/main/java/org/test/lib/Hello.java
@@ -0,0 +1,9 @@
+package org.test.lib;
+
+public class Hello {
+	
+	public static void sayHello(String name) {
+		System.out.println("Hello "+name);
+	}
+
+}
--- org.springsource.ide.eclipse.gradle.core.test/resources/projects/non-exported-deps/main/src/main/java/org/test/main/Main.java
+++ org.springsource.ide.eclipse.gradle.core.test/resources/projects/non-exported-deps/main/src/main/java/org/test/main/Main.java
@@ -1,11 +1,13 @@
 package org.test.main;
 
 import org.apache.commons.collections.buffer.CircularFifoBuffer;
+import org.test.lib.Hello;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		new CircularFifoBuffer();
+		Hello.sayHello("Kris");
 	}
 
 }
