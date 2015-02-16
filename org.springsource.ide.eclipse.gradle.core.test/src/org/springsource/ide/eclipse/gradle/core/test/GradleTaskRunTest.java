f40acf8033db935270225e6ff4b5989f2f45abb4
Kris De Volder <kdevolder@pivotal.io>
2015-02-16 09:52:50 -0800

    Add 2.2 to 'run tasks with different releases' test

--- org.springsource.ide.eclipse.gradle.core.test/src/org/springsource/ide/eclipse/gradle/core/test/GradleTaskRunTest.java
+++ org.springsource.ide.eclipse.gradle.core.test/src/org/springsource/ide/eclipse/gradle/core/test/GradleTaskRunTest.java
@@ -699,7 +699,7 @@ public class GradleTaskRunTest extends GradleTest {
 		}
 		String[] versions = {  
 				"1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "1.10", "1.11", "1.12",
-				"2.0", "2.1"
+				"2.0", "2.1", "2.2"
 		};
 		Result[] results = new Result[versions.length];
 		boolean failed = false;
